e035a466b1e7ba848ff142ee3d81e10601cced21
Francisco Orrequia Terrero <forrequia@atsistemas.com>
2018-06-20 09:56:23 +0200

    Creación de la estructura

---
+++ src/main/java/com/fo/SpringWithMongo/SpringWithMongoApplication.java
@@ -0,0 +1,12 @@
+package com.fo.SpringWithMongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithMongoApplication.class, args);
+	}
+}
